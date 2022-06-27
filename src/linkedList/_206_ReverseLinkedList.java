1ecb1c715e8331e51c173a206b02d796dd8b7f28
Li, Diyang <diyangli233@gmail.com>
2022-06-27 10:10:32 -0400

    reverseLinkedList

---
+++ src/linkedList/_206_ReverseLinkedList.java
@@ -0,0 +1,63 @@
+package linkedList;
+
+/**
+ * @author Diyang Li
+ * @create 2022-06-27 9:19 AM
+ */
+public class _206_ReverseLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+    public static ListNode reverseList2(ListNode head) {
+        if(head == null){
+            return null;
+        }
+
+        ListNode pre = null;
+        ListNode next = null;
+        ListNode cur = head;
+
+        while (cur != null){
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+
+        node = reverseList(node);
+    }
+}
