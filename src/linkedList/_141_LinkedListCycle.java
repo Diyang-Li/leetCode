61158f8d57fe5e4a72c7c0dd6c4e3b4adb880b24
Li, Diyang <diyangli233@gmail.com>
2022-07-02 20:47:29 -0400

    Linked List Cycle

--- src/linkedList/_141_LinkedListCycle.java
+++ src/linkedList/_141_LinkedListCycle.java
@@ -39,5 +39,27 @@ public class _141_LinkedListCycle {
         return true;
     }
 
+    /**
+     * Another better method
+     * @param head
+     * @return
+     */
+    public static boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
 
 }
