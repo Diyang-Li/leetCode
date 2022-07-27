994336dcf6b33760de9f9d68485e428e1c67ca29
Li, Diyang <diyangli233@gmail.com>
2022-07-27 17:25:21 -0400

    Serialize and Deserialize Binary Tree

---
+++ src/tree/_297_SerializeandDeserializeBinaryTree.java
@@ -0,0 +1,75 @@
+package tree;
+
+import com.sun.source.tree.Tree;
+
+import javax.management.Query;
+import java.security.spec.RSAMultiPrimePrivateCrtKeySpec;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Diyang Li
+ * @create 2022-07-27 3:47 PM
+ */
+public class _297_SerializeandDeserializeBinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    static StringBuilder sb = new StringBuilder();
+
+    /**
+     * preorder
+     * @param root
+     * @return
+     */
+    // Encodes a tree to a single string.
+    public static String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        return rserialize(root, sb).toString();
+    }
+
+    public static StringBuilder rserialize(TreeNode root, StringBuilder sb) {
+        if (root == null) sb.append("#,");
+        else {
+            sb.append(root.val);
+            sb.append(",");
+            sb = rserialize(root.left, sb);
+            sb = rserialize(root.right, sb);
+        }
+        return sb;
+    }
+
+    public static TreeNode deserialize(String data) {
+        String[] array = data.split(",");
+        Queue<String> queue = new LinkedList<>(Arrays.asList(array));
+        return rdeserialize(queue);
+    }
+
+    public static TreeNode rdeserialize(Queue<String> queue){
+        String s = queue.poll();
+        if(s.equals("#")) return null;
+        TreeNode treeNode = new TreeNode(Integer.parseInt(s));
+        treeNode.left = rdeserialize(queue);
+        treeNode.right = rdeserialize(queue);
+        return treeNode;
+    }
+
+    public static void main(String[] args) {
+        TreeNode head = new TreeNode(1);
+        head.left = new TreeNode(2);
+        head.right = new TreeNode(5);
+        head.left.left = new TreeNode(3);
+        head.left.right = new TreeNode(4);
+
+        System.out.println(deserialize(serialize(head)).val);
+    }
+}
