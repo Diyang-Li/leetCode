456c956d778ecac6ad451955c08500c6947f75c5
Li, Diyang <diyangli233@gmail.com>
2022-07-31 11:03:10 -0400

    7/31/2022

---
+++ src/tree/template/_173_BinarySearchTreeIterator.java
@@ -0,0 +1,53 @@
+package tree.template;
+
+import java.util.Stack;
+
+/**
+ * @author Diyang Li
+ * @create 2022-07-31 10:34 AM
+ */
+public class _173_BinarySearchTreeIterator {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    Stack<TreeNode> stack;
+
+    public _173_BinarySearchTreeIterator(TreeNode root) {
+        stack = new Stack<>();
+        pushNode(root);
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+        pushNode(node.right);
+        return node.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    public void pushNode(TreeNode root) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+
+    }
+}
