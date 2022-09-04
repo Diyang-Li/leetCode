6fbf1bf5e4a6ba0633e8ee658953e5a8b24c46bb
Li, Diyang <diyangli233@gmail.com>
2022-09-03 22:34:31 -0400

    09/03/2022

---
+++ src/sort/_148_SortList.java
@@ -0,0 +1,60 @@
+package sort;
+
+/**
+ * @author Diyang Li
+ * @create 2022-09-03 10:33 PM
+ */
+public class _148_SortList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode mid = findMid(head);
+        ListNode right = sortList(mid.next);
+        mid.next = null;
+        ListNode left = sortList(head);
+        return merge(left, right);
+    }
+
+    private ListNode findMid(ListNode head) {
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+
+    }
+}
